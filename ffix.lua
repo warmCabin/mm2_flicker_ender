c18132481bfc2dab22ed963e5575f24cb9108b99
warmCabin <ryanwb2@gmail.com>
2021-10-22 19:25:11 -0400

    make ffix respect quotes and escaped quotes in args

--- ffix.lua
+++ ffix.lua
@@ -3,13 +3,18 @@
     - print does not support \n
     - io.write does absolutely nothing
     - arg is a plain string instead of a table
-    - arg is often a nil reference
+    - arg is often a nil reference (usually when loading a script with the emulator paused)
     - os.exit closes the emulator
 ]]
 
+if type(arg) == "table" then
+    print("arg is already correct.\nffix is not necessary in your environment!")
+    return
+end
+
 local fceuxPrint = print
 
--- print does not respect newlines!!
+-- FCEUX print does not respect newlines!
 function print(...)
     for _, v in ipairs(arg) do
         if type(v) == "string" then
@@ -27,7 +32,7 @@ function print(...)
     end
 end
 
--- io.write does nothing!!!
+-- FCEUX io.write does nothing!
 io.write = print
 
 -- Somehow, this doesn't always get called :/
@@ -38,13 +43,58 @@ end
 -- arg can occasionally go missing, ending up as a nil reference.
 assert(arg, "Command line arguments got lost somehow :(\nPlease run this script again.")
 
--- FCEUX passes arg as a string, but vanilla Lua does a table. Sometimes, this string can be null if you are unlucky.
-local argT = {}
-for str in arg:gmatch("[^ ]+") do
-    table.insert(argT, str) -- table.pack does not exist in FCEUX. I don't know what to say...
+local function clean(str)
+    local ret = ""
+    local start = 1
+    for j = 1, #str do
+        if str:sub(j, j) == '"' then
+            if j ~= 1 and str:sub(j - 1, j - 1) == '\\' then
+                -- leave quote, remove backslash
+                ret = ret..str:sub(start, j - 2)..'"'
+                start = j + 1
+            else
+                -- remove quote
+                ret = ret..str:sub(start, j - 1)
+                start = j + 1
+            end
+        end
+    end
+    
+    if start <= #str then
+        ret = ret..str:sub(start, #str)
+    end
+    
+    return ret
 end
 
--- Info at stack level 3 should be the script that required this file.
+-- FCEUX passes arg as a single string, but it's supposed to be a table.
+local argT = {}
+
+local quoted = false
+local start = 1
+local stop
+for i = 1, #arg do
+    if arg:sub(i, i) == '"' and (i == 1 or arg:sub(i - 1, i - 1) ~= '\\') then
+        quoted = not quoted
+    elseif not quoted and arg:sub(i, i) == " " then
+        stop = i - 1
+        if stop >= start then
+            table.insert(argT, clean(arg:sub(start, stop)))
+        end
+        start = i + 1
+    end
+end
+
+stop = #arg
+if stop >= start then table.insert(argT, clean(arg:sub(start, stop))) end
+
+for i, str in ipairs(argT) do
+    print(string.format("[%d] |%s|", i, str))
+end
+
+-- Setup some metadata that's supposed to be in the arg table.
+-- [-1] (and below) = name and args of interpreter. "fceux" seemes fine for this.
+-- [0] = script name. Info at stack level 3 should be the script that required this file.
 local info = debug.getinfo(3, "S")
 local name = info.source:match("[^/\\]+\.lua")
 argT[-1] = "fceux"
